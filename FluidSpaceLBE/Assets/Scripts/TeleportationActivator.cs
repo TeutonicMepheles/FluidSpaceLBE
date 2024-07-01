f6b63df614d531d4eb973b5d6e446d7724a2a03b
狗兰家自耕农 <65597661+TeutonicMepheles@users.noreply.github.com>
2024-07-01 22:52:14 +0800

    #240701 粗糙的逻辑实现
    目前用了非常投机的方法实现了边界管理的预期效果，但snap volume定位到anchor的功能还没有实现，后续是可以优化这个部分的。下周预计的工作是在现在这个逻辑的基础上研究纯基于代码的生成方式

--- FluidSpaceLBE/Assets/Scripts/TeleportationActivator.cs
+++ FluidSpaceLBE/Assets/Scripts/TeleportationActivator.cs
@@ -6,7 +6,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class TeleportationActivator : MonoBehaviour
 {
     public LayerMask teleAreaLayerMask; // TeleArea层的LayerMask
-    public GameObject teleportation; // Teleportation GameObject
+    public XRRayInteractor[] teleportation; // Teleportation GameObject
 
     void Update()
     {
@@ -14,22 +14,27 @@ public class TeleportationActivator : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(transform.position, Vector3.down, out hit, 2f, teleAreaLayerMask, QueryTriggerInteraction.Collide))
         {
-            // 如果Raycast命中TeleArea层，则启用Teleportation GameObject
-            if (teleportation != null && !teleportation.activeSelf)
+            for (int i = 0; i < teleportation.Length; i++)
             {
-                teleportation.SetActive(true);
-                Debug.Log("Teleportation GameObject enabled");
+                // 如果Raycast命中TeleArea层，则启用Teleportation GameObject
+                if (teleportation[i] != null && !teleportation[i].enabled)
+                {
+                    teleportation[i].enabled = true;
+                    Debug.Log("Teleportation GameObject enabled");
+                }
             }
         }
         else
         {
-            // 如果Raycast没有命中TeleArea层，则禁用Teleportation GameObject
-            if (teleportation != null && teleportation.activeSelf)
+            for (int i = 0; i < teleportation.Length; i++)
             {
-                teleportation.SetActive(false);
-                Debug.Log("Teleportation GameObject disabled");
+                // 如果Raycast没有命中TeleArea层，则禁用Teleportation GameObject
+                if (teleportation[i] != null && teleportation[i].enabled)
+                {
+                    teleportation[i].enabled = false;
+                    Debug.Log("Teleportation GameObject disabled");
+                }
             }
         }
     }
-    
 }
